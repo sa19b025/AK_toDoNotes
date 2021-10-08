61a386c12f930d350182d22450faa2e5a01e9ce9
sa19b025 <82029788+sa19b025@users.noreply.github.com>
2021-10-08 04:52:17 +0200

    bootstrap integration

--- script.js
+++ script.js
@@ -2,7 +2,36 @@ var neuesElement = 0;
 var toDoArray = new Array();
 const button = document.getElementById("add-todo");
 button.addEventListener("click", buttonPress);
+
 function buttonPress (event){
-    neuesElement = document.getElementById("todoNote");
-    neuesElement.push("<li>" + toDoArray + "</li>");
+    addToDo();
 }
+
+function handleKeyDown(event) {
+    
+    if (event.key === "Enter") {
+        addToDo();
+    }
+}
+
+function addToDo() {
+
+    const list = document.getElementById("todo-list");
+
+    const input = document.getElementById("todo-note");
+    const text = input.value;
+  
+
+    const item = document.createElement("li");
+    item.textContent = text;
+    item.addEventListener("click", removeToDo);
+    input.value = "";
+    input.focus();
+  
+    list.appendChild(item);
+  }
+  
+  function removeToDo(event) {
+    const list = document.getElementById("liste");
+    list.removeChild(event.target);
+  }
