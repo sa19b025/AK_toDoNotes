2aced8cbe7a153c325faced09f4e2ea95224f7f8
sa19b025 <82029788+sa19b025@users.noreply.github.com>
2021-10-14 18:13:17 +0200

    -

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
-    const arrayFromStorage = JSON.parse(window.localStorage.getItem('toDoArray'));
-    console.log(arrayFromStorage);
+  const btnAddToDo = window.document.getElementById("add-todo");
+  btnAddToDo.addEventListener("click", buttonPress);
 
-    const btnAddToDo = window.document.getElementById("add-todo");
-    btnAddToDo.addEventListener("click", buttonPress);
-  
-    const input = document.getElementById("todoNote");
-    input.addEventListener("keydown", handleKeyDown);
-  });
+  const input = document.getElementById("todoNote");
+  input.addEventListener("keydown", handleKeyDown);
+
+  getListFromStorage();
+
+});
+
+arrayFromStorage = new Array();
+arrayFromStorage = JSON.parse(localStorage.getItem('toDoArray'));
+storageArray = new Array();
+
+if(arrayFromStorage != "") {
+      storageArray = arrayFromStorage;
+  }
 
 
 function buttonPress (event){
-    addToDo();
+  addToDo();
 }
 
 function handleKeyDown(event) {
-    
-    if (event.key === "Enter") {
-        addToDo();
-    }
+  
+  if (event.key === "Enter") {
+      addToDo();
+  }
 }
-  
-function addToDo() {
 
-    const list = document.getElementById("liste");
-    
-    const input = document.getElementById("todoNote");
-    const text = input.value;
-    
-    const item = document.createElement("li");
-    item.textContent = text;
-    item.classList.add("list-group-item");
-    item.addEventListener("click", removeToDo);
-    input.value = "";
-    input.focus();
+function getListFromStorage(){
+  const list = document.getElementById("liste");
+  const input = document.getElementById("todoNote");
   
-    list.appendChild(item);
-    window.localStorage.setItem('toDoArray', JSON.stringify(list));
+  for (let i = 0; i < arrayFromStorage.length; i++) {
+      let text = arrayFromStorage[i];
+      const item = document.createElement("li");
+      item.classList.add("list-group-item");
+      item.addEventListener("click", removeToDo);
+
+      item.textContent = text;
+      list.appendChild(item);
   }
+}
+
+function addToDo() {
+  const list = document.getElementById("liste");
+  const input = document.getElementById("todoNote");
+  const text = input.value;
+
+  const item = document.createElement("li");
+  item.textContent = text;
+  item.classList.add("list-group-item");
+  item.addEventListener("click", removeToDo);
+  list.appendChild(item);
+
+  storageArray[storageArray.length] = text;
   
-  function removeToDo(event) {
-    const list = document.getElementById("liste");
-    list.removeChild(event.target);
-    window.localStorage.setItem('toDoArray', JSON.stringify(list));
-  }
+  localStorage.setItem('toDoArray', JSON.stringify(storageArray));
+
+  input.value = "";
+  input.focus();
+}
+
+function removeToDo(event) {
+  const list = document.getElementById("liste");
+  list.removeChild(event.target);
+}
